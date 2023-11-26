a8a495b13030a5fd6a6d2a2365131547475fe4da
Павел <kurinkov.ps@gmail.com>
2023-11-27 00:49:35 +0300

    app v 1.0 build

--- src/component/Header/Header.tsx
+++
@@ -1,6 +0,0 @@
-import { IPropsHeader } from "../interfaces/interfaces";
-import styles from "./Header.module.scss";
-const Header = ({ children }: IPropsHeader) => {
-  return <div className={styles.HeaderContainer}>{children}</div>;
-};
-export default Header;
